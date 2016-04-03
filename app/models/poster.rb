d9c6bff5313a672301e5a506b39ec595023e1202
David Ladowitz <david@ladowitz.com>
2016-04-02 20:43:25 -0700

    updating job postings

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -22,7 +22,7 @@ class Poster < ActiveRecord::Base
     approved_colleges.each do |college|
       puts "\nCollge is: #{college.home_url}"
 
-      if college.job_postings.where(title: job[:title], company: job[:company], desta_url: job[:desta_url]).any?
+      if college.job_postings.where(title: job[:title], company: job[:company]).any?
         puts "Skipping - Job has already been posted for #{job[:title]} - #{job[:company]} - #{job[:desta_url]}"
         duplicate_job_posting_for_college += 1
         next
@@ -181,7 +181,7 @@ class Poster < ActiveRecord::Base
     jobs[0] = {
       title: "Summer Camp Counselor",
       company: "Indian Head Camp",
-      desta_url: "desta.co\/job\/opt-specialist",
+      desta_url: "desta.co\/job\/opt-specialist/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push",
       salary: "$2,400/mo + Room & Board",
       location_wanted: ["PA"],
       region: "Equinunk",
@@ -200,7 +200,7 @@ class Poster < ActiveRecord::Base
       company_zip: "18431",
       expire_date: "06\/20\/2016",
 
-      job_description: "https:\/\/desta.co/job/opt-specialist <br><br>
+      job_description: "<a href=\"https:\/\/desta.co/job/opt-specialist/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push\">Apply Here</a> <br><br>
 Counselors are the guides and facilitators at Indian Head Camp. OPT staff work with all ages at camp and with a variety of skills, including boating, zipping, climbing, mountain biking and hiking to name a few.<br><br>
 The program starts with educational nature hikes around camp and builds up to a four-day trip to the Adirondacks that includes camping on an island.  The design of the program is to increase the challenges as our campers need and age increases.<br><br>
 On camp, the programs offers everything from a 30 element project adventure ropes course, 1,215 feet of zipline, 7 miles of hiking and mountain biking trails, rock climbing walls, bouldering walls, nature education, daily Delaware River canoe trips, and a variety of special outdoor adventure-centric camp events. Off camp, is really where the adventure begins.<br><br>
@@ -211,7 +211,7 @@ As they get older, they begin taking overnight trips to the beautiful Adirondack
     jobs[1] = {
       title: "Off-Road Tour Guide",
       company: "Pink Jeep Tours",
-      desta_url: "desta.co\/job\/tour-guide",
+      desta_url: "desta.co\/job\/tour-guide/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push",
       salary: "Depends on Experience",
       location_wanted: ["AZ"],
       region: "Tusayan",
@@ -230,7 +230,7 @@ As they get older, they begin taking overnight trips to the beautiful Adirondack
       company_zip: "86023",
       expire_date: "06\/20\/2016",
 
-      job_description: "https:\/\/desta.co/job/tour-guide <br><br>
+      job_description: "<a href=\"https:\/\/desta.co/job/tour-guide/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push\">Apply Here</a> <br><br>
 Pink Jeep Tours has been thrilling visitors as the premier off-road excursion company in the Southwest for more than 50 years. The first Jeep tour operator in the United States, the company was founded in Sedona, Arizona in 1960 and has since become known for its “must do” rugged adventure tours through Sedona’s Red Rock Country, the Grand Canyon, Scottsdale, and Las Vegas.<br><br>
 Pink Jeep Tours is seeking Tour Guides. This position will work in our beautiful Grand Canyon location and provide educational and fun tours at one of the most treasured sites in the United States. Join a great team of professionals who love what they do, love the product, and love working for a company that sells FUN!<br><br>
 Job Responsibilities and Requirements:<br>
@@ -244,7 +244,7 @@ Compensation and Benefits:<br>
     jobs[2] = {
       title: "River Guide",
       company: "Oregon River Experiences",
-      desta_url: "desta.co\/job\/o-r-e-river-guide",
+      desta_url: "desta.co\/job\/o-r-e-river-guide/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push",
       salary: "$75 - $150 a day",
       location_wanted: ["OR"],
       region: "Beavercreek",
@@ -263,7 +263,7 @@ Compensation and Benefits:<br>
       company_zip: "97004",
       expire_date: "06\/20\/2016",
 
-      job_description: "https:\/\/desta.co/job/o-r-e-river-guide <br><br>
+      job_description: "<a href=\"https:\/\/desta.co/job/o-r-e-river-guide/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push\">Apply Here</a> <br><br>
       Founded in 1978, Oregon River Experiences conducts half day to nine day whitewater raft trips on ten rivers in Oregon and Idaho. One of the Northwest’s premier outfitters, O.R.E.’s participatory raft trips focus on experiential learning and personal involvement. We employ guides who possess the work ethic, maturity, social aptitude, integrity, leadership skills, and whitewater boating skills necessary to conduct high quality river trips.<br><br>
 
       General responsibilities:<br>
@@ -292,7 +292,7 @@ Compensation and Benefits:<br>
     jobs[3] = {
       title: "Alaskan Outdoor Guide",
       company: "Kodiak Raspberry Island Remote Lodge",
-      desta_url: "desta.co\/job\/guide-kodiad-raspberry-island-remote-lodge",
+      desta_url: "desta.co\/job\/guide-kodiad-raspberry-island-remote-lodge/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push",
       salary: "$2,850.00/mo Tips/Room/Board",
       location_wanted: ["AK"],
       region: "Kodiak",
@@ -311,7 +311,7 @@ Compensation and Benefits:<br>
       company_zip: "99615",
       expire_date: "06\/20\/2016",
 
-      job_description: "https:\/\/desta.co/job/guide-kodiad-raspberry-island-remote-lodge <br><br>
+      job_description: "<a href=\"https:\/\/desta.co/job/guide-kodiad-raspberry-island-remote-lodge/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push\">Apply Here</a> <br><br>
       The Guide Position is a combination of two sub-positions, each starting at 8:00am, and concluding at 6:00pm. First, the Guide will primarily be in charge of any kayaking and or hiking expeditions, and secondly, if the lodge doesn’t have kayakers/hikers scheduled, the guide will work as a deckhand on the boat. Lastly, the Guide will be prepared and available for ‘other duties as assigned,’ as needed.<br><br>
 The Kayaking and Hiking Guide’s focus will be on taking our guests interested in those activities out on such trips, and support and follow-through of those day’s programs. Guests are typically ready for their day of activities between 8:30 and 9am. This position is client-to-guide relationship intensive--you must start ‘guiding’ right away. Our guests vary in physical ability and experience level, so be prepared to mold the experience you will offer your participants to their ability and enthusiasm. Be prepared to teach the participants every aspect of the trip they are on--how to put on a kayak skirt properly, how to hold a paddle and stroke properly, how to dress properly based on the weather and the type of trip you are taking them on. Be prepared to answer questions regarding the locale; the flora, fauna, and other questions relating to the weather, tides, geography, etc. Be prepared to educate and position the group for good photographs. We do not expect you to know every answer but have the resources here so you can gather the majority of the information you’ll need to provide an educational and rewarding experience to your participants.<br><br>
 The weather here is predictable to a degree. You will need to make decisions based on the safety of the group and our equipment that may conflict with your participant’s immediate goals. You will be guiding in bear country and will need to be comfortable with every stage of a bear encounter, including contact. We can teach you this, but if you’re terrified of bears or the water, this is not the job for you. If you are terrified of bears or the water and want this job to overcome your fear, this is not the job for you. Trips nearly always start and finish here at the lodge. Rare exceptions will involve a boat drop off and/or pick up. There are a variety of trails, almost always game trails, that go up the mountain behind the lodge or along the coast. There are a variety of kayak experiences that start from the lodge here, as well, and will always very as the tide comes in and goes out. Though we have a very gentle current, water height variation between high and low tides may be as much as 24 feet. It is also common to paddle a ways, get out, and enjoy some time on a local beach, or walk/hike from there. We require guides and guests return to the lodge by 5:00pm. In some cases guests will wish to either depart the lodge later in the morning or return to the lodge earlier in the evening. It is imperative that the Guide remains “guiding,” or “in charge” of his or her group of guests until 5:30pm; the remaining team at the lodge does not have room in their schedule to interact with the guests. The lodge itself is also closed from 1:00 p.m. to 4:00 p.m. Guides are expected to keep our equipment well maintained, clean, and ready for the next day’s activities.<br><br>
@@ -322,7 +322,7 @@ On both kayaking/hiking guide days as well as deckhand days, the guide is expect
     jobs[4] = {
       title: "Wilderness Ranch Packer",
       company: "Triple J Ranch",
-      desta_url: "desta.co\/job\/wilderness-packer",
+      desta_url: "desta.co\/job\/wilderness-packer/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push",
       salary: "Depends on experience",
       location_wanted: ["MT"],
       region: "Augusta",
@@ -341,7 +341,7 @@ On both kayaking/hiking guide days as well as deckhand days, the guide is expect
       company_zip: "59410",
       expire_date: "06\/20\/2016",
 
-      job_description: "https:\/\/desta.co/job/wilderness-packer <br><br>
+      job_description: "<a href=\"https:\/\/desta.co/job/wilderness-packer/?utm_source=collegecentral&utm_medium=jobboards&utm_campaign=bot-push\">Apply Here</a> <br><br>
       Each year we look for exceptional individuals to help us provide a warm, caring, safe and stress free environment for the guests that we serve. Our employees are important to us and become part of the family. We strive for excellence and the best service in the industry so we look for employees with the same qualities. If you are a self-proclaimed slacker, stop reading now. However, if you are a self-motivated, high energy, problem-solving person with a passion for the outdoors and people, we would like to hear from you!<br><br>
 We average around 20 guests per week at the Ranch and 8-10 guests on the pack-trips. Our size allows us to give each guest special attention. Often our guests say that this was the best vacation they have ever had, and it is our goal that this is true for every guest. To ensure that goal, we seek employees who are mature, outgoing individuals with an excellent work ethic, good moral character and excellent communication skills. We expect a ready smile, willingness to work hard and have fun, and a love of the outdoors and people. This requires a self-starting individual with physical stamina to put in long hours and hard work. All employees need to be organized, willing to take initiative, and have a “can do” attitude.<br><br>
 RESPONSIBILITIES:<br>
